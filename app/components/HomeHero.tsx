59676b00271f170c71c13c9bfa174bfa41d82463
Joshua Linog <jsinkcreations@gmail.com>
2024-11-15 13:29:33 -0500

    change bkg of hero

--- app/components/HomeHero.tsx
+++ app/components/HomeHero.tsx
@@ -19,16 +19,20 @@ const HomeHero: React.FC = () => {
     }, []);
 
     return (
-        <div className="w-full h-screen bg-no-repeat bg-cover bg-center bg-[url('https://images.unsplash.com/photo-1552196563-55cd4e45efb3?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw4fHx5b2dhfGVufDB8MHx8fDE3MTY5NjgwNTN8MA&ixlib=rb-4.0.3&q=80&w=1080')]">
+        <div className="w-full h-screen bg-no-repeat bg-cover bg-center bg-[url('https://raw.githubusercontent.com/joshualinog/charlestontables/refs/heads/main/home-hero-background.jpg')]">
             <header className="lg:px-16 px-4 flex flex-wrap items-center py-4 shadow-sm">
                 <div className="flex-1 flex justify-between items-center mix-blend-screen">
-                    <a href="#" className="text-4xl text-gray-400 font-extrabold">YOGA</a>
+                    <a href="#" >
+                        <strong className="text-1xl text-gray-400 font-extrabold">CHARLESTON</strong> <br />
+                        <strong className="text-3xl text-gray-400 font-extrabold">TABLES</strong>
+                    </a>
+
                 </div>
 
                 <label htmlFor="menu-toggle" className="pointer-cursor md:hidden block">
                     <svg className="fill-current text-gray-600" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
                         <title>menu</title>
-                        <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+                        <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
                     </svg>
                 </label>
                 <input className="hidden" type="checkbox" id="menu-toggle" />
@@ -49,10 +53,11 @@ const HomeHero: React.FC = () => {
             <div className="w-[90%] mx-auto h-full flex items-center justify-between py-10">
                 <div className="lg:w-fit">
                     <div className="sm:text-6xl xs:text-5xl text-left text-white font-serif font-extrabold uppercase">
-                        <h1>Get</h1>
-                        <h1>in</h1>
-                        <h1 className="bg-black/30 text-white rounded-sm px-1 shadow-sm shadow-white/50">Health</h1>
-                        <h1>Today</h1>
+                        <h1>Set</h1>
+                        <h1>the</h1>
+                        <h1>Table</h1>
+                        <h1 className="bg-black/30 text-white rounded-sm px-1 shadow-sm shadow-white/50">Charleston</h1>
+
                     </div>
                     <div className="w-full flex items-center justify-between mt-6 py-1 px-4 uppercase bg-green-500 rounded-sm">
                         <h3 className="text-white text-lg font-semibold">join now</h3>
